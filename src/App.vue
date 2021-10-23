366286008475d1291fbf84183f921b9153c2f2ea
Elmira Naghibi <elmiranaghibi@gmail.com>
2021-10-23 16:59:06 +0330

    fea : action buttons

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,104 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="con">
+      <div class="health">
+        <div class="per player" :style="{ width: playerhealth + '%' }">
+          {{ playerhealth }}
+        </div>
+      </div>
+      <div class="health">
+        <div class="per" :style="{ width: monsterhealth + '%' }">
+          {{ monsterhealth }}
+        </div>
+      </div>
+    </div>
+    <div class="con">
+      <div class="center" v-if="!gameIsRunning">
+        <button @click="startGame">start new game</button>
+      </div>
+      <div class="center" v-else>
+        <button @click="attack">attack</button>
+        <button @click="specialattack">special Attack</button>
+        <button @click="help">heal</button>
+        <button @click="giveup">give up</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+
+  data() {
+    return {
+      playerhealth: 100,
+      monsterhealth: 100,
+      gameIsRunning: false,
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {
+    startGame: function () {
+      this.gameIsRunning = true;
+      this.playerhealth = 100;
+      this.monsterhealth = 100;
+    },
+    giveup: function () {
+      this.gameIsRunning = false;
+    },
+    help: function () {
+      if(this.playerhealth <= 90) {
+        this.playerhealth += 10;
+      } else {
+        this.playerhealth = 100;
+      }
+      this.monsterAttack();
+    },
+
+    specialattack: function () {
+      this.monsterhealth -= this.calculateDamage(10,20);
+      if(this.checkWin()){
+        return;
+      }
+      this.monsterAttack();
+    },
+
+    attack: function () {
+      this.monsterhealth -= this.calculateDamage(3,10);
+      if(this.checkWin()){
+        return;
+      }
+      this.monsterAttack();
+    },
+    monsterAttack: function() {
+      this.playerhealth -= this.calculateDamage(5,12)
+      this.checkWin();
+    },
+    calculateDamage: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function () {
+      if (this.monsterhealth <= 0) {
+        if (confirm("you won!! new game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerhealth <= 0) {
+        if (confirm("you lost!!!!!!!!!!!!!!!!!!! new game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +109,42 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.con {
+  border: 1px solid salmon;
+  font-size: 0;
+}
+.center {
+  text-align: center;
+  border: 1px solid;
+}
+.health {
+  width: calc(50% - 22px);
+  font-size: 25px;
+  font-weight: bold;
+  display: inline-block;
+  margin: 10px;
+  border: 1px solid lightslategrey;
+  border-radius: 7px;
+}
+.per {
+  height: 50px;
+  line-height: 50px;
+  background: lightpink;
+  border-radius: 7px;
+}
+
+button {
+  height: 50px;
+  line-height: 50px;
+  background: skyblue;
+  outline: none;
+  border: none;
+  margin: 5px;
+  width: 200px;
+  border-radius: 7px;
+}
+.player {
+  background: lightgreen;
 }
 </style>
---
+++ src/components/headerPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ index+1 }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headerPage',
+  props :{
+    index: Number
+  }
+}
+</script>
+
+<style scoped>
+h1{
+  font-size: 50px;
+}
+</style>
