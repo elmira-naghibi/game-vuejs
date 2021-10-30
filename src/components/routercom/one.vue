cc7c3dec3152e5051f4cc237ce9b8bf07208e6c1
Elmira Naghibi <elmiranaghibi@gmail.com>
2021-10-30 17:57:55 +0330

    fea : childeren routes

--- src/components/routercom/one.vue
+++ src/components/routercom/one.vue
@@ -1,17 +1,25 @@
 <template>
     <div style="font-size:45px;border:1px solid red; width:200px; margin:10px; padding:10px; cursor:pointer">
        <h1> one </h1>
-       <p>{{ id }}</p>
-       <button @click="gototwo()">go to two</button>
+           <router-view></router-view>
+
+       <!-- <p>{{ id }}</p> -->
+       <!-- <button @click="gototwo()">go to two</button> -->
     </div>
 </template>
 <script>
 export default {
  data() {
      return {
-         id: this.$route.params.id
+        //  id: this.$route.params.id
          
      }
+ },
+ watch:{
+     "$route"(to, from){
+         this.id=to.params.id;
+     }
+
  },
  methods: {
      gototwo:function(){
--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,13 @@ import home from './components/routercom/home.vue';
 
 export const routes = [
     { path: '', component: home },
-    { path: '/one/:id', component: one },
-    { path: '/two', component: two },
-    { path: '/three', component: three },
-    { path: '/four', component: four }
+    { path: '/one', component: one , children : [
+        { path:'two', component:two},
+        { path:'three', component:three},
+        { path:':id', component:four},
+        ]
+    },
+    // { path: '/two', component: two },
+    // { path: '/three', component: three },
+    // { path: '/four', component: four }
 ];
