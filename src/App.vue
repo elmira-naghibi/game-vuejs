5beac626032b1965e68f5e733ecabef2155f8164
Elmira Naghibi <elmiranaghibi@gmail.com>
2021-10-30 16:39:31 +0330

    fea: use exact in links in vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <!-- <li>
-      <router-link to="/">Home</router-link>
+    <li>
+      <router-link to="/" active-class="active" exact><a>Home</a></router-link>
     </li>
     <li>
-      <router-link to="/one">one</router-link>
+      <router-link to="/one" active-class="active"><a>one</a></router-link>
     </li>
     <li>
-      <router-link to="/two">two</router-link>
+      <router-link to="/two"><a>two</a></router-link>
     </li>
     <li>
-      <router-link to="/three">three</router-link>
+      <router-link to="/three"><a>three</a></router-link>
     </li>
     <li>
-      <router-link to="/four">four</router-link>
+      <router-link to="/four"><a>four</a></router-link>
     </li>
     <li>
-      <router-link to="/five">five</router-link>
-    </li> -->
+      <router-link to="/five"><a>five</a></router-link>
+    </li>
     <router-view></router-view>
   </div>
 </template>
@@ -47,5 +47,8 @@ li{
   display: inline-block;
   font-size: 32px;
   font-weight: bold;
+}
+.active{
+  background: lightgreen;
 }
 </style>
