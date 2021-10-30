d1fee642c43663b5039a3095e40d4134067e6319
Elmira Naghibi <elmiranaghibi@gmail.com>
2021-10-30 16:47:00 +0330

    fea: push in ruter in js in project vue

--- src/components/routercom/one.vue
+++ src/components/routercom/one.vue
@@ -1,5 +1,20 @@
 <template>
     <div style="font-size:45px;border:1px solid red; width:200px; margin:10px; padding:10px; cursor:pointer">
        <h1> one</h1>
+       <button @click="gototwo()">go to two</button>
     </div>
 </template>
+<script>
+export default {
+ data() {
+     return {
+         
+     }
+ },
+ methods: {
+     gototwo:function(){
+         this.$router.push("/three")
+     }
+ },
+}
+</script>
